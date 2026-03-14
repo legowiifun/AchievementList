8902a6b08556a7bfcbff8de1423cb9d6dc8bcb2d
legowiifun <arath5@gmail.com>
2026-03-14 15:17:22 -0700

    Keep a copy of json files and image files in an easy to modify resources directory

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,10 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    extraResource: [
+      './resources',
+      './src/json'
+    ]
   },
   rebuildConfig: {},
   makers: [
