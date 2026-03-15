be9fb66592fc8f670262573fa74a4fbbf435c220
legowiifun <arath5@gmail.com>
2026-03-15 13:23:39 -0700

    Fix some bugs

--- src/js/initialize.js
+++ src/js/initialize.js
@@ -42,10 +42,11 @@ export class initialize {
             return;
         }
         for (let i=0;i<this.gamesJson.length;i++) {
+            //add a new game
             this.myGames.push(new Game(this.gamesJson[i].name, this.gamesJson[i].img, this.gamesJson[i].platform));
             let jsonName=this.gamesJson[i].jsonName;
             window.resources.getJson(jsonName).then((result)=>{
-                this.initGame(JSON.parse(result));
+                this.initGame(JSON.parse(result), i);
             }).catch((err)=> {
                 console.error("Failed to read "+jsonName+"!");
             });
@@ -56,17 +57,17 @@ export class initialize {
     /**
      * @param {object[]} game 
      */
-    initGame(game) {
+    initGame(game, idx) {
         if (game.length==undefined) {
             console.error("JSON file is not an array!");
             return;
         }
         for (let i=0;i<game.length;i++) {
-            this.myGames.at(-1).addAchievementSet(game[i].name,game[i].image);
+            this.myGames.at(idx).addAchievementSet(game[i].name,game[i].image);
             for (let j=0;j<game[i].achievements.length;j++) {
                 //console.log("i=",i);
                 //console.log("Adding to achievement set ",this.myGames.at(-1).achievementSets[i]);
-                this.myGames.at(-1).addAchievementByIndex(i,game[i].achievements[j].name,game[i].achievements[j].description,game[i].achievements[j].img,game[i].achievements[j].unlocked,game[i].achievements[j].unlockDate);
+                this.myGames.at(idx).addAchievementByIndex(i,game[i].achievements[j].name,game[i].achievements[j].description,game[i].achievements[j].img,game[i].achievements[j].unlocked,game[i].achievements[j].unlockDate);
             }
         }
     }
