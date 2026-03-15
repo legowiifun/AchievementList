3cc1af3d276edbece14569a54f8c7a9e8f8d8c5c
legowiifun <arath5@gmail.com>
2026-03-14 17:34:14 -0700

    Better store the resources path for easier access.

--- src/js/initialize.js
+++ src/js/initialize.js
@@ -2,9 +2,18 @@ import { Game } from './game.js';
 import games from '../../resources/games.json' with {type: "json"};
 import TestGame1 from '../../resources/TestGame1.json' with {type: "json"};
 export class initialize {
+    resourcesPath;
+    main() {
+        window.electron.getPath.then((result)=>{
+            this.resourcesPath=result;
+        }).then(()=>{
+            this.init();
+        });
+    }
+
     init() {
-        
-        window.electron.getPath.then((result)=>{console.log(result)});
+        //window.electron.getPath.then((result)=>{console.log(result)});
+        console.log(this.resourcesPath);
         for (let i=0;i<games.length;i++) {
             this.myGames.push(new Game(games[i].name, games[i].img, games[i].platform));
             let jsonName=games[i].jsonName;
@@ -15,6 +24,7 @@ export class initialize {
         }
         console.log(this.myGames);
     }
+
     initGame(game) {
         for (let i=0;i<game.length;i++) {
             this.myGames.at(-1).addAchievementSet(game[i].name,game[i].image);
