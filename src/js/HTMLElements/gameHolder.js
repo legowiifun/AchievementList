7c54d55d948c510bf2f025b5e1894adc26cc5b90
legowiifun <arath5@gmail.com>
2026-03-15 11:25:48 -0700

    No longer pass just the index to the gameHolders

--- src/js/HTMLElements/gameHolder.js
+++ src/js/HTMLElements/gameHolder.js
@@ -1,13 +1,12 @@
 export class GameHolder {
     myHTML;
-    constructor(idx) {
-        let myGames = window.initialize.myGames;
+    constructor(game) {
         let newHTML = "<li>";
-        window.resources.getCompletePath(myGames[idx].img).then((result)=>{
+        window.resources.getCompletePath(game.img).then((result)=>{
             newHTML=newHTML+"<img wdith=\"100\" height=\"100\" src=\""+result+"\">";
-            newHTML=newHTML+"<span class=\"gameName\">"+myGames[idx].name+"</span>";
-            newHTML=newHTML+"<span class=\"platform\">"+myGames[idx].platform+"</span>";
-            newHTML=newHTML+"<span class=\"percent\" id=\""+myGames[idx].name+myGames[idx].platform+"Percent\">"+myGames[idx].getPercentageCompleted()+"%</span>";
+            newHTML=newHTML+"<span class=\"gameName\">"+game.name+"</span>";
+            newHTML=newHTML+"<span class=\"platform\">"+game.platform+"</span>";
+            newHTML=newHTML+"<span class=\"percent\" id=\""+game.name+game.platform+"Percent\">"+game.getPercentageCompleted()+"%</span>";
             newHTML=newHTML+"</li>";
             this.myHTML=newHTML;
             document.getElementById("gamesList").innerHTML=document.getElementById("gamesList").innerHTML+this.myHTML;
--- src/js/views/gameViewer.js
+++ src/js/views/gameViewer.js
@@ -4,7 +4,7 @@ export class GameViewer {
         let HTML = "<ul id=\"gamesList\"></ul>";
         document.getElementById("content").innerHTML=HTML;
         for (let i=0;i<gamesArray.length;i++) {
-            new GameHolder(i);
+            new GameHolder(gamesArray[i]);
         }
     }
 }
