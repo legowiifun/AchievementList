63a576b16248b23c1fb3eba6d487f58ca7719b07
legowiifun <arath5@gmail.com>
2026-03-15 10:49:41 -0700

    Shrink down the game image

--- src/js/HTMLElements/gameHolder.js
+++ src/js/HTMLElements/gameHolder.js
@@ -4,7 +4,7 @@ export class GameHolder {
         let myGames = window.initialize.myGames;
         let newHTML = "<li>";
         window.resources.getCompletePath(myGames[idx].img).then((result)=>{
-            newHTML=newHTML+"<img src=\""+result+"\">";
+            newHTML=newHTML+"<img wdith=\"100\" height=\"100\" src=\""+result+"\">";
             newHTML=newHTML+"<span class=\"gameName\">"+myGames[idx].name+"</span>";
             newHTML=newHTML+"<span class=\"platform\">"+myGames[idx].platform+"</span>";
             newHTML=newHTML+"<span class=\"percent\" id=\""+myGames[idx].name+myGames[idx].platform+"Percent\">"+myGames[idx].getPercentageCompleted()+"%</span>";
