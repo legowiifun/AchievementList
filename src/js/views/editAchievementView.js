1d3344844a6e928402d065600c05741bb826afd7
legowiifun <arath5@gmail.com>
2026-03-21 09:34:16 -0700

    Add some input validation to editing unlocked dates, and added time input support.

--- src/js/views/editAchievementView.js
+++ src/js/views/editAchievementView.js
@@ -6,24 +6,35 @@ export class EditAchievementView {
     * @param {Achievement} achievement
     */
     constructor(achievement) {
-        let newHTML="<form>"
-            newHTML=newHTML+"<p> Date: ";
-                newHTML=newHTML+"<input type=\"date\" id=\"editAchievementDate\">";
-            newHTML=newHTML+"</p>";
-            newHTML=newHTML+"<p> Obtained?: ";
-                newHTML=newHTML+"<input type=\"checkbox\" id=\"editAchievementCheckbox\">";
-            newHTML=newHTML+"</p>";
-            newHTML=newHTML+"<button id=\"achievementEditBtn\""
-                newHTML=newHTML+">DONE</button>";
-        newHTML=newHTML+"</form>";
+        let newHTML="<p> Date: ";
+            newHTML=newHTML+"<input type=\"datetime-local\" id=\"editAchievementDate\">";
+        newHTML=newHTML+"</p>";
+        newHTML=newHTML+"<p> Obtained?: ";
+            newHTML=newHTML+"<input type=\"checkbox\" id=\"editAchievementCheckbox\">";
+        newHTML=newHTML+"</p>";
+        newHTML=newHTML+"<button id=\"achievementSaveBtn\""
+            newHTML=newHTML+">DONE</button>";
         document.getElementById("content").innerHTML=newHTML;
         
         //add the event listener to the button
-        document.getElementById("achievementEditBtn").addEventListener("click",()=> {
-            achievement.unlockDate=document.getElementById("editAchievementDate").valueAsDate;
-            achievement.unlocked=document.getElementById("editAchievementCheckbox").checked;
-            editJson(achievement.achievementSet.game.saveJSONLocation,achievement.achievementSet.game.createSaveJSON());
-            window.initialize.setView(window.initialize.views.achievementsView, window.initialize.achievementSetIdx);
+        document.getElementById("achievementSaveBtn").addEventListener("click",()=> {
+            let newDate=new Date(document.getElementById("editAchievementDate").value);
+            let newUnlocked=document.getElementById("editAchievementCheckbox").checked;
+            if (newUnlocked) {
+                if (!isNaN(newDate.getTime())) {
+                    achievement.unlockDate=newDate;
+                    achievement.unlocked=newUnlocked;
+                    editJson(achievement.achievementSet.game.saveJSONLocation,achievement.achievementSet.game.createSaveJSON());
+                    window.initialize.setView(window.initialize.views.achievementsView, window.initialize.achievementSetIdx);
+                } else {
+
+                }
+            } else {
+                achievement.unlocked=newUnlocked;
+                achievement.unlockDate=undefined;
+                editJson(achievement.achievementSet.game.saveJSONLocation,achievement.achievementSet.game.createSaveJSON());
+                window.initialize.setView(window.initialize.views.achievementsView, window.initialize.achievementSetIdx);
+            }
         });
     }
 }
