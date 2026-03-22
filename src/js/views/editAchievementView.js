4f21e3b641c534b11d3da1f53b2785b168f4f0f9
legowiifun <arath5@gmail.com>
2026-03-22 09:51:21 -0700

    Refactor editing achievements to use document.createElement

--- src/js/views/editAchievementView.js
+++ src/js/views/editAchievementView.js
@@ -7,7 +7,8 @@ export class EditAchievementView {
     * @param {Achievement} achievement
     */
     constructor(achievement) {
-        let newHTML="<p> Date: ";
+
+        /*let newHTML="<p> Date: ";
             newHTML=newHTML+"<input type=\"datetime-local\" id=\"editAchievementDate\">";
         newHTML=newHTML+"</p>";
         newHTML=newHTML+"<p> Obtained?: ";
@@ -15,12 +16,32 @@ export class EditAchievementView {
         newHTML=newHTML+"</p>";
         newHTML=newHTML+"<button id=\"achievementSaveBtn\""
             newHTML=newHTML+">DONE</button>";
-        document.getElementById("content").innerHTML=newHTML;
+        document.getElementById("content").innerHTML=newHTML;*/
         
+        let dateParagraph=document.createElement('p');
+        dateParagraph.innerHTML="Date: ";
+        let editAchievementdate=document.createElement('input');
+        editAchievementdate.type="datetime-local";
+        editAchievementdate.id="editAchievementDate";
+        dateParagraph.appendChild(editAchievementdate);
+        document.getElementById("content").appendChild(dateParagraph);
+
+        let checkboxParagraph=document.createElement('p');
+        checkboxParagraph.innerHTML="Date: ";
+        let editAchievementCheckbox=document.createElement('input');
+        editAchievementCheckbox.type="checkbox";
+        editAchievementCheckbox.id="editAchievementCheckbox";
+        checkboxParagraph.appendChild(editAchievementCheckbox);
+        document.getElementById("content").appendChild(checkboxParagraph);
+
+        let achievementSaveBtn = document.createElement('button');
+        achievementSaveBtn.id="achievementSaveBtn";
+        achievementSaveBtn.innerHTML="DONE";
+
         //add the event listener to the button
-        document.getElementById("achievementSaveBtn").addEventListener("click",()=> {
-            let newDate=new Date(document.getElementById("editAchievementDate").value);
-            let newUnlocked=document.getElementById("editAchievementCheckbox").checked;
+        achievementSaveBtn.addEventListener("click",()=> {
+            let newDate=new Date(editAchievementdate.value);
+            let newUnlocked=editAchievementCheckbox.checked;
             if (newUnlocked) {
                 if (!isNaN(newDate.getTime())) {
                     achievement.unlockDate=newDate;
@@ -37,5 +58,6 @@ export class EditAchievementView {
                 windowAPI.viewManager.setView(windowAPI.viewManager.views.achievementsView, windowAPI.viewManager.achievementSetIdx);
             }
         });
+        document.getElementById("content").appendChild(achievementSaveBtn);
     }
 }
