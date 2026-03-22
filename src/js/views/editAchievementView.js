227e6bb63d6f896f37b6a5c770b1e8f3c3dbd54b
legowiifun <arath5@gmail.com>
2026-03-22 13:31:08 -0700

    Add an input box for outOf

--- src/js/views/editAchievementView.js
+++ src/js/views/editAchievementView.js
@@ -7,17 +7,6 @@ export class EditAchievementView {
     * @param {Achievement} achievement
     */
     constructor(achievement) {
-
-        /*let newHTML="<p> Date: ";
-            newHTML=newHTML+"<input type=\"datetime-local\" id=\"editAchievementDate\">";
-        newHTML=newHTML+"</p>";
-        newHTML=newHTML+"<p> Obtained?: ";
-            newHTML=newHTML+"<input type=\"checkbox\" id=\"editAchievementCheckbox\">";
-        newHTML=newHTML+"</p>";
-        newHTML=newHTML+"<button id=\"achievementSaveBtn\""
-            newHTML=newHTML+">DONE</button>";
-        document.getElementById("content").innerHTML=newHTML;*/
-        
         let dateParagraph=document.createElement('p');
         dateParagraph.innerHTML="Date: ";
         let editAchievementdate=document.createElement('input');
@@ -27,12 +16,22 @@ export class EditAchievementView {
         document.getElementById("content").appendChild(dateParagraph);
 
         let checkboxParagraph=document.createElement('p');
-        checkboxParagraph.innerHTML="Date: ";
+        checkboxParagraph.innerHTML="Unlocked: ";
         let editAchievementCheckbox=document.createElement('input');
         editAchievementCheckbox.type="checkbox";
         editAchievementCheckbox.id="editAchievementCheckbox";
         checkboxParagraph.appendChild(editAchievementCheckbox);
         document.getElementById("content").appendChild(checkboxParagraph);
+        let editAchievementOutofInput=undefined;
+        if (achievement.outOf!=undefined) {
+            let outOfParagraph=document.createElement('p');
+            outOfParagraph.innerHTML="Completed out of: ";
+            editAchievementOutofInput=document.createElement('input');
+            editAchievementOutofInput.type="number";
+            editAchievementOutofInput.id="editAchievementOutofInput";
+            outOfParagraph.appendChild(editAchievementOutofInput);
+            document.getElementById("content").appendChild(outOfParagraph);
+        }
 
         let achievementSaveBtn = document.createElement('button');
         achievementSaveBtn.id="achievementSaveBtn";
@@ -42,10 +41,12 @@ export class EditAchievementView {
         achievementSaveBtn.addEventListener("click",()=> {
             let newDate=new Date(editAchievementdate.value);
             let newUnlocked=editAchievementCheckbox.checked;
+            let newOutOf=editAchievementOutofInput.valueAsNumber;
             if (newUnlocked) {
-                if (!isNaN(newDate.getTime())) {
+                if (!isNaN(newDate.getTime())&&(!isNaN(newOutOf)||achievement.outOf==undefined)) {
                     achievement.unlockDate=newDate;
                     achievement.unlocked=newUnlocked;
+                    achievement.doneOutOf=newOutOf;
                     editJson(achievement.achievementSet.game.saveJSONLocation,achievement.achievementSet.game.createSaveJSON());
                     windowAPI.viewManager.setView(windowAPI.viewManager.views.achievementsView, windowAPI.viewManager.achievementSetIdx);
                 } else {
