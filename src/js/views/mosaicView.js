ed53ae3ab5cebddc2f54d1421a2e127ec218740f
legowiifun <arath5@gmail.com>
2026-04-05 14:49:07 -0700

    Create mosaic using Canvas element

--- src/js/views/mosaicView.js
+++ src/js/views/mosaicView.js
@@ -1,22 +1,224 @@
 import { Game } from "../game.js";
 import { windowAPI } from '../APIThroughWindow.js';
+import { getCompletePath } from "../utils.js";
 export class MosaicViewer {
+    /**
+     * @type {string[]}
+     */
+    platforms=[];
     /**
      * 
      * @param {Game[]} gamesArray 
      */
     constructor(gamesArray) {
+        let self=this;
         //platform filter
-        
+        //selection box
+        //can select multiple
+        //if select other, create list of others, any size
+        let platformParagraph=document.createElement('p');
+        platformParagraph.id="platformParagraph";
+        platformParagraph.innerHTML="Platforms to view (leave empty for all): ";
+        let platformSelect = document.createElement('select');
+        platformSelect.id="achievementSelectPlatform";
+        platformSelect.multiple=true;
+        let options = ["PlayStation 3", "PlayStation 4", "PlayStation Vita", "PlayStation 5", "XBox", "XBox 360", "XBox One", "XBox Series X", "Steam", "GOG", "Other"];
+        for (let i=0;i<options.length;i++) {
+            let optionSelect = document.createElement('option');
+            optionSelect.value=options[i];
+            optionSelect.innerHTML=options[i];
+            platformSelect.appendChild(optionSelect);
+        }
+        platformSelect.size=1;
+
+        let platformList = document.createElement('ul');
+        platformList.hidden=true;
+
+        let platformTextInputCount = document.createElement('input');
+        platformTextInputCount.type="number";
+        platformTextInputCount.id="platformCountTextInput";
+        platformTextInputCount.hidden=true;
+        platformSelect.addEventListener('change', function(event) {
+            let flag=false;
+            for (let i=0;i<platformSelect.selectedOptions.length&&!flag;i++) {
+                if (platformSelect.selectedOptions.item(i).innerText=="Other") {
+                    platformTextInputCount.hidden=false;
+                    platformList.hidden=false;
+                    flag=true;
+                }
+            }
+            if (!flag) {
+                platformTextInputCount.hidden=true;
+                platformList.hidden=true;
+            }
+            self.platforms=Array.from(platformSelect.selectedOptions).map((option)=> {
+                if (option!="Other") {
+                    return option.innerText;
+                }
+            });
+        });
+        platformTextInputCount.addEventListener('change',function(event) {
+            platformList.innerHTML="";
+            for (let i=0;i<platformTextInputCount.value;i++) {
+                let platformTextInput=document.createElement('input');
+                platformTextInput.type="text";
+                platformTextInput.id="addGameTextInput";
+                platformTextInput.addEventListener('change', function() {
+                    self.platforms[i+platformSelect.selectedOptions.length]=platformTextInput.value;
+                });
+                platformList.appendChild(document.createElement('li').appendChild(platformTextInput));
+            }
+        });
+        platformParagraph.appendChild(platformSelect);
+        platformParagraph.appendChild(platformTextInputCount);
+        platformParagraph.appendChild(platformList);
+        platformParagraph.classList.add("formElement");
+        windowAPI.mainContent.appendChild(platformParagraph);
+
         //percentage threshold/have plat
+        let havePlatParagraph = document.createElement('p');
+        havePlatParagraph.id="havePlatParagraph";
+        havePlatParagraph.innerText="Percentage filter - check for if you are checking for the platinum";
+        let havePlatCheckbox=document.createElement('input');
+        havePlatCheckbox.name="havePlatCheckbox";
+        havePlatCheckbox.id="havePlatCheckbox";
+        havePlatCheckbox.type="checkbox";
+        let percentageInput = document.createElement('input');
+        percentageInput.name="percentageInput";
+        percentageInput.id="percentageInput";
+        percentageInput.type="number";
+        percentageInput.min=0;
+        percentageInput.max=100;
+        havePlatCheckbox.addEventListener('change', function(event) {
+            if (havePlatCheckbox.checked) {
+                percentageInput.hidden=true;
+            } else {
+                percentageInput.hidden=false;
+            }
+        });
+        havePlatParagraph.appendChild(havePlatCheckbox);
+        havePlatParagraph.appendChild(percentageInput);
+        havePlatParagraph.classList.add("formElement");
+        windowAPI.mainContent.appendChild(havePlatParagraph);
 
         //number of columns/automatic fit
+        let columnCountParagraph = document.createElement('p');
+        columnCountParagraph.id="columnCountParagraph";
+        columnCountParagraph.innerText="Column count - Check to automatically fit";
+        let autoFitCheckbox=document.createElement('input');
+        autoFitCheckbox.name="autoFitCheckbox";
+        autoFitCheckbox.id="autoFitCheckbox";
+        autoFitCheckbox.type="checkbox";
+        let columnCountInput = document.createElement('input');
+        columnCountInput.name="columnCountInput";
+        columnCountInput.id="columnCountInput";
+        columnCountInput.type="number";
+        columnCountInput.min=0;
+        autoFitCheckbox.addEventListener('change', function(event) {
+            if (autoFitCheckbox.checked) {
+                columnCountInput.hidden=true;
+            } else {
+                columnCountInput.hidden=false;
+            }
+        });
+        columnCountParagraph.appendChild(autoFitCheckbox);
+        columnCountParagraph.appendChild(columnCountInput);
+        columnCountParagraph.classList.add("formElement");
+        windowAPI.mainContent.appendChild(columnCountParagraph);
 
         //create mosaic button
-
+        let createBtn = document.createElement('button');
+        createBtn.id="createBtn";
+        createBtn.innerText="Create Mosaic!";
+        windowAPI.mainContent.appendChild(createBtn);
         // canvas
         let canvasElement = document.createElement('canvas');
+        canvasElement.width=3000;
+        canvasElement.height=1500;
         
+        createBtn.addEventListener('click',(event)=> {
+            let games = gamesArray.filter((value)=> {
+                //first filter by platform
+                if (this.platforms.length!=0) {
+                    let flag=false;
+                    for (let i=0;i<this.platforms.length&&!flag;i++) {
+                        if (this.platforms[i]==value.platform) {
+                            flag=true;
+                        }
+                    }
+                    //if the platform is not in the platforms list
+                    if (!flag) {
+                        return false;
+                    }
+                }
+                //filter by percent
+                if (havePlatCheckbox.checked) {
+                    if (value.havePlat()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if (value.getPercentageCompleted()>=percentageInput.valueAsNumber) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            });
+
+            windowAPI.sortGames(false,"Last Obtained Achievement Date Descending",games);
+
+            let rows;
+            let columns;
+            if (autoFitCheckbox.checked) {
+                //TODO: algorithm to automatically calculate rows and columns
+                columns=10;
+                rows=Math.ceil(games.length/columns);
+            } else {
+                columns=columnCountInput.valueAsNumber;
+                rows=Math.ceil(games.length/columns);
+            }
+            let gameSize=625;
+            canvasElement.width=gameSize*columns;
+            canvasElement.height=gameSize*rows;            
+            //TODO: create canvas
+            let context = canvasElement.getContext("2d");
+            let gameIdx = 0;
+            //self.createImgTag(games[gameIdx],context,0,0,gameSize);
+            if (windowAPI.viewConsoleLogs) {
+                console.log("Creating mosaic. Game size=",gameSize,"Rows=",rows,"Columns=",columns);
+                console.log("Games=",games.length);
+            }
+            for (let i=0;(i<columns)&&(gameIdx<games.length);i++) {
+                if (windowAPI.viewConsoleLogs) {
+                    console.log("Going through canvas column ",i);
+                }
+                //go through each column, and each row
+                for (let j=0;j<(rows)&&(gameIdx<games.length);j++) {
+                    if (windowAPI.viewConsoleLogs) {
+                        console.log("Adding ",games[gameIdx]," to canvas at location ",gameSize*i,gameSize*j, "size=",gameSize);
+                    }
+                    self.createImgTag(games[gameIdx],context,gameSize*i,gameSize*j,gameSize);
+                    
+                    gameIdx++;
+                }
+                //context.drawImage(createimgTag(games[gameIdx]),0,0,10,10);
+            }
+        });
+
+        //save button
+        let saveCanvas = document.createElement('button');
+        saveCanvas.innerText="Save";
+        saveCanvas.addEventListener('click', (event)=> {
+            let link = document.createElement('a');
+            let img = canvasElement.toDataURL('image/png');
+            link.href=img;
+            link.download='mosaic-img.png';
+            link.click();
+        });
+        windowAPI.mainContent.appendChild(saveCanvas);
+        windowAPI.mainContent.appendChild(document.createElement('br'));
         windowAPI.mainContent.appendChild(canvasElement);
         
     }
@@ -25,10 +227,15 @@ export class MosaicViewer {
      * @param {Game} game 
      * @return {HTMLImageElement}
      */
-    createImgTag(game) {
+    createImgTag(game,context,locationX,locationY,size) {
         let imgTag = document.createElement("img");
-        imgTag.src=game.platImg;
-        
+        getCompletePath(game.platImg).then((result)=>{
+            imgTag.src=result;
+            imgTag.width=size;
+            imgTag.height=size;
+        }).then(()=> {
+            context.drawImage(imgTag,locationX,locationY,size,size);
+        });
         return imgTag;
     }
 }
