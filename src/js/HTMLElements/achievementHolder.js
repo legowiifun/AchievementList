32db60b961958cfd595e445303d7e518378664e3
legowiifun <arath5@gmail.com>
2026-05-02 16:19:21 -0700

    Change the achievement icon to a lock if the achievement is locked

--- src/js/HTMLElements/achievementHolder.js
+++ src/js/HTMLElements/achievementHolder.js
@@ -15,15 +15,11 @@ export class AchievementHolder {
         let achievementEntry = document.createElement('li');
         achievementEntry.classList.add("achievementEntry");
         achievementEntry.classList.add("displayHolder");
-        
-        getCompletePath(achievement.img).then((result)=>{
+        if (achievement.hidden) {
             let achievementImg = document.createElement('img');
             achievementImg.height=100;
-            achievementImg.src=result;
+            achievementImg.src="../Default Resources/Hidden Achievement.png";
             achievementImg.classList.add("achievementImg");
-            if (!achievement.unlocked) {
-                achievementImg.classList.add("unobtainedAchImg");
-            }
             achievementEntry.appendChild(achievementImg);
 
             let achievementName = document.createElement('span');
@@ -56,6 +52,48 @@ export class AchievementHolder {
             achievementEntry.appendChild(achievementEdit);
 
             parentElement.appendChild(achievementEntry);
-        });
+        } else {
+            getCompletePath(achievement.img).then((result)=>{
+                let achievementImg = document.createElement('img');
+                achievementImg.height=100;
+                achievementImg.src=result;
+                achievementImg.classList.add("achievementImg");
+                if (!achievement.unlocked) {
+                    achievementImg.classList.add("unobtainedAchImg");
+                }
+                achievementEntry.appendChild(achievementImg);
+
+                let achievementName = document.createElement('span');
+                achievementName.classList.add("achievementName");
+                achievementName.innerText=achievement.name;
+                achievementEntry.appendChild(achievementName);
+
+                let achievementDesc = document.createElement('span');
+                achievementDesc.classList.add("achievementDesc");
+                achievementDesc.innerText=achievement.description;
+                achievementEntry.appendChild(achievementDesc);
+                
+                let achievementDate = document.createElement('span');
+                achievementDate.classList.add("achievementDate");
+                if (achievement.unlocked) {
+                    achievementDate.innerText=convertDate(achievement.unlockDate,0);
+                } else {
+                    achievementDate.innerText="Unobtained";
+                }
+                achievementEntry.appendChild(achievementDate);
+
+                
+
+                let achievementEdit = document.createElement('button');
+                achievementEdit.id="achievementEdit";
+                achievementEdit.innerText="Edit";
+                achievementEdit.addEventListener('click', ()=> {
+                    windowAPI.viewManager.setView(windowAPI.viewManager.views.editAchievementView, idx);
+                });
+                achievementEntry.appendChild(achievementEdit);
+
+                parentElement.appendChild(achievementEntry);
+            });
+        }
     }
 }
