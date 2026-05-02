fefa864f784b9fbb599d04059f85e993c1e24658
legowiifun <arath5@gmail.com>
2026-05-02 16:27:20 -0700

    Refactor to no longer use separate achievementHolder class

--- src/js/views/achievementViewer.js
+++ src/js/views/achievementViewer.js
@@ -1,11 +1,109 @@
 import {AchievementHolder} from '../HTMLElements/achievementHolder.js'
+import { Achievement } from "../achievement.js";
+import { getCompletePath, convertDate } from "../utils.js";
+import { windowAPI } from "../APIThroughWindow.js"
+
 export class AchievementViewer {
     constructor(achievementArray) {
         let achievementList = document.createElement('ul');
         achievementList.id="achievementList";
         for (let i=0;i<achievementArray.length;i++) {
-            new AchievementHolder(achievementArray[i], i,achievementList);
+            //new AchievementHolder(achievementArray[i], i,achievementList);
+            achievementList.appendChild(this.createAchievementHolder(achievementArray[i],i));
         }
         document.getElementById("content").appendChild(achievementList);
+    }
+    /**
+     * 
+     * @param {Achievement} achievement 
+     * @param {int} idx 
+     * @returns 
+     */
+    createAchievementHolder(achievement, idx) {
+        this.achievement=achievement;
+        let achievementEntry = document.createElement('li');
+        achievementEntry.classList.add("achievementEntry");
+        achievementEntry.classList.add("displayHolder");
+        if (achievement.hidden) {
+            let achievementImg = document.createElement('img');
+            achievementImg.height=100;
+            achievementImg.src="../Default Resources/Hidden Achievement.png";
+            achievementImg.classList.add("achievementImg");
+            achievementEntry.appendChild(achievementImg);
+
+            let achievementName = document.createElement('span');
+            achievementName.classList.add("achievementName");
+            achievementName.innerText=achievement.name;
+            achievementEntry.appendChild(achievementName);
+
+            let achievementDesc = document.createElement('span');
+            achievementDesc.classList.add("achievementDesc");
+            achievementDesc.innerText=achievement.description;
+            achievementEntry.appendChild(achievementDesc);
+            
+            let achievementDate = document.createElement('span');
+            achievementDate.classList.add("achievementDate");
+            if (achievement.unlocked) {
+                achievementDate.innerText=convertDate(achievement.unlockDate,0);
+            } else {
+                achievementDate.innerText="Unobtained";
+            }
+            achievementEntry.appendChild(achievementDate);
+
+            
+
+            let achievementEdit = document.createElement('button');
+            achievementEdit.id="achievementEdit";
+            achievementEdit.innerText="Edit";
+            achievementEdit.addEventListener('click', ()=> {
+                windowAPI.viewManager.setView(windowAPI.viewManager.views.editAchievementView, idx);
+            });
+            achievementEntry.appendChild(achievementEdit);
+
+            return achievementEntry;
+        } else {
+            getCompletePath(achievement.img).then((result)=>{
+                let achievementImg = document.createElement('img');
+                achievementImg.height=100;
+                achievementImg.src=result;
+                achievementImg.classList.add("achievementImg");
+                if (!achievement.unlocked) {
+                    achievementImg.classList.add("unobtainedAchImg");
+                }
+                achievementEntry.appendChild(achievementImg);
+
+                let achievementName = document.createElement('span');
+                achievementName.classList.add("achievementName");
+                achievementName.innerText=achievement.name;
+                achievementEntry.appendChild(achievementName);
+
+                let achievementDesc = document.createElement('span');
+                achievementDesc.classList.add("achievementDesc");
+                achievementDesc.innerText=achievement.description;
+                achievementEntry.appendChild(achievementDesc);
+                
+                let achievementDate = document.createElement('span');
+                achievementDate.classList.add("achievementDate");
+                if (achievement.unlocked) {
+                    achievementDate.innerText=convertDate(achievement.unlockDate,0);
+                } else {
+                    achievementDate.innerText="Unobtained";
+                }
+                achievementEntry.appendChild(achievementDate);
+
+                
+
+                let achievementEdit = document.createElement('button');
+                achievementEdit.id="achievementEdit";
+                achievementEdit.innerText="Edit";
+                achievementEdit.addEventListener('click', ()=> {
+                    windowAPI.viewManager.setView(windowAPI.viewManager.views.editAchievementView, idx);
+                });
+                achievementEntry.appendChild(achievementEdit);
+
+                
+            });
+            return achievementEntry;
+        }
     }
 }
